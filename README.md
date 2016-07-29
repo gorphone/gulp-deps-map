db12b927639327b2ce33f14e31b0088bd367acde
gaofeng13 <gaofeng13@meituan.com>
2016-07-29 17:25:01 +0800

    upgrade to support file not with angular

--- README.md
+++ README.md
@@ -1,4 +1,85 @@
-# gulp-deps-map
+a plugin to analysis the dependance of angular module, the ouput is a json like this:
+```
+{
+    "flow.provider": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-provider.js",
+        "deps": []
+    },
+    "flow.init": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-init.js",
+        "deps": [
+            "flow.provider"
+        ]
+    },
+    "flow.btn": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-btn.js",
+        "deps": [
+            "flow.init"
+        ]
+    },
+    "flow.dragEvents": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-dragevents.js",
+        "deps": [
+            "flow.init"
+        ]
+    },
+    "flow.drop": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-drop.js",
+        "deps": [
+            "flow.init"
+        ]
+    },
+    "flow.events": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-events.js",
+        "deps": [
+            "flow.init"
+        ]
+    },
+    "flow.img": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-img.js",
+        "deps": [
+            "flow.init"
+        ]
+    },
+    "flow.transfers": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-transfers.js",
+        "deps": [
+            "flow.init"
+        ]
+    },
+    "flow": {
+        "md5": "ff57bcde125f45a9402010f0078baecb",
+        "type": "js",
+        "path": "common/lib/node_modules/ng-flow/dist/ng-flow-standalone.js",
+        "deps": [
+            "flow.provider",
+            "flow.init",
+            "flow.events",
+            "flow.btn",
+            "flow.drop",
+            "flow.transfers",
+            "flow.img",
+            "flow.dragEvents"
+        ]
+    }
+```
+To do so, you can manage or load you js file in project of angular as you like.
 
 To install:
 ```
@@ -6,21 +87,45 @@ npm install gulp-deps-map --save
 ```
 
 usage:
-
 ```
 var gulpDepsMaps = require('gulp-deps-map');
-var filter = require('gulp-filter');
-
-var isAdded = function (file) {
-    return file.event === 'add';
-};
-var filterAdded = filter(isAdded);
-
+var config = {
+	 "alias": {
+        "common/lib/node_modules/jquery/dist/jquery.js": "jquery",
+        "common/lib/node_modules/angular/angular.js": "angular",
+        "common/lib/node_modules/bootstrap/dist/js/bootstrap.js": "bootstrap",
+        "common/lib/no_npm/ht-form-validition/src/validator.js": "validator",
+        "common/lib/no_npm/ht-form-validition/src/jquery.validator.js": "jquery.validator",
+        "common/lib/no_npm/select2/select2.js": "jquery.select2",
+        "common/lib/no_npm/jquery.panzoom/dist/jquery.panzoom.js": "jquery.panzoom",
+        "common/lib/no_npm/jQuery.dragmove/jQuery.dragmove.js": "jquery.dragmove",
+        "common/lib/no_npm/resizeThis/resizeThis.js": "resizeThis"
+    },
+    "shim": {
+        "ui.select2": {
+            "deps": [
+                "jquery.select2"
+            ]
+        },
+        "ng-light-box" : {
+            "deps": [
+                "jquery",
+                "jquery.panzoom", 
+                "jquery.dragmove", 
+                "jquery.mousewheel", 
+                "resizeThis"
+            ]
+        }
+    }
+}
+ 
 gulp.src('./src/**/*.js')
     .pipe(watch('./src/**/*.js'))
     .pipe(filterAdded)
-    .pipe(gulpDepsMaps())
+    .pipe(gulpDepsMaps(config))
     .pipe(gulp.dest('./public/src/'));
 ```
 
-use gulp-deps-loader to load dependences
+the other files which are writed in angular can be managed by `alias` and `shim` in config 
+
+use `gulp-deps-loader` to load dependences
--- index.js
+++ index.js
@@ -4,130 +4,170 @@ var es = require('event-stream');
 var path = require('path');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
-var strip = require('gulp-strip-comments');
-    // urljoin = require("url-join"),
-    // trumpet = require("trumpet"),
-    // concat  = require("concat-stream");
+// var strip = require('gulp-strip-comments');
+/**
+ * {
+    "alias": {
+        "common/lib/node_modules/jquery/dist/jquery.js": "jquery",
+        "common/lib/node_modules/angular/angular.js": "angular",
+    },
+    "shim": {
+            "ui.select2": {
+                "deps": [
+                    "jquery.select2"
+                ]
+            }
+        }
+    }
+ * @type {Object}
+ */
+var config = {};
 
-var file = 'file-deps.json';
-var map = {
-	'file_name': {
-		type: 'js',
-		path: 'uri',
-		deps: [
-		]
-	}
-}
+var _unique_trim = function (array) {
+    var n = []; // 临时数组
+    var keys = {};
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] && !keys[array[i]]) {
+            n.push(array[i]);
+            keys[array[i]] = true;
+        }
+    }
+    return n;
+};
 
 var getDeps = function (file) {
-	var R = /angular\s*\.\s*module\s*\(\s*[\'\"]([\w\d_\.-]+)[\'\"]\s*,\s*\[([^\]]*)\]\s*\)/gm;
+    var relativePath = path.relative(file.base, file.path);
+    var R = /@ng-map-deps:\s*\[([^\]]*)\]\s*/gm;
+    var alias = config.alias || {};
+    var shim = config.shim || {};
 
-	var content = String(file.contents);
-	var deps = [];
-	var matchs;
+    var content = String(file.contents);
+    var deps = [];
+    var matchs;
+    var comment_deps = [];
+    var shim_deps = [];
+    var _id;
+    var _deps = [];
 
-	// var md5 = crypto.createHash('md5').update(content).digest('hex');
+    var md5 = crypto.createHash('md5').update(content).digest('hex');
 
-	//console.log(content);
-	while(matchs = R.exec(content)){
-		if(matchs.length){
-			deps.push({
-				id: matchs[1].trim(),
-				// md5: md5,
-				type: 'js',
-				path: path.relative(file.base, file.path),
-				deps: matchs[2].replace(/['"\r\n\s*]/g, '').split(',')
-			})
-		}
-	}
-	return deps;
-}
+    matchs = R.exec(content);
+    if (matchs && matchs.length) {
+        comment_deps = matchs[1].replace(/['"\r\n\s*]/g, '').split(',');
+    }
+
+    if (alias[relativePath]) {
+        _id = alias[relativePath];
+        if (shim[_id]) {
+            shim_deps = shim[_id].deps;
+        }
+        deps.push({
+            id: _id,
+            md5: md5,
+            type: 'js',
+            path: relativePath,
+            deps: _unique_trim(_deps.concat(shim_deps).concat(comment_deps))
+        });
+    } else {
+        R = /(^|\W|\s)angular\s*\.\s*module\s*\(\s*['"]([\w\d_\.-]+)['"]\s*,\s*\[([^\]]*)\]\s*\)/gm;
+        // angular的模块
+        while ((matchs = R.exec(content))) {
+            if (matchs.length) {
+                _id = matchs[2].trim();
+                _deps = matchs[3].replace(/['"\r\n\s*]/g, '').split(',');
+                if (shim[_id]) {
+                    shim_deps = shim[_id].deps;
+                }
+                deps.push({
+                    id: _id,
+                    md5: md5,
+                    type: 'js',
+                    path: relativePath,
+                    deps: _unique_trim(_deps.concat(shim_deps).concat(comment_deps))
+                });
+            }
+        }
+    }
+
+    return deps;
+};
 
 var getDepsFiles = function (base) {
-	return function getDepsFile(file, cb) {
+    return function getDepsFile (file, cb) {
+        if (file.processedByTempDepsMap) {
+            return cb(null, file);
+        }
 
-		if (file.processedByTempDepsMap) {
-	      return cb(null, file);
-	    }
+        if (file.isNull()) {
+            return cb(null, file);
+        }
 
-		if (file.isNull()) {
-	      return cb(null, file);
-	    }
+        if (file.isStream()) {
+            return cb(new gutil.PluginError('gulp-deps-map', 'doesn\'t support Streams'));
+        }
 
-	    if (file.isStream()) {
-	      return cb(new gutil.PluginError('gulp-deps-map', 'doesn\'t support Streams'));
-	    }
+        var deps = getDeps(file);
+        var str = [];
 
-	    var deps = getDeps(file);
-	    var str = [];
+        deps.forEach(function (dep) {
+            if (dep && dep.id) {
+                str.push(JSON.stringify(dep));
+            }
+        });
+        file.processedByTempDepsMap = true;
 
-	    deps.forEach(function (dep) {
-	    	if(dep && dep.id){
-	    		str.push(JSON.stringify(dep));
-	    	}
-	    });
-		file.processedByTempDepsMap = true;
+        if (str.length) {
+            file.contents = new Buffer(str.join('\n'));
+            cb(null, file);
+        } else {
+            cb();
+        }
+        return null;
+    };
+};
 
-		if(str.length){
-			file.contents = new Buffer(str.join('\n'));
-			cb(null, file);
-		}else{
-			cb();
-		}
-		
-	}
-}
-
-var depsMap = function ( base ) {
-	return es.map(getDepsFiles(base));
-} 
+var depsMap = function (base) {
+    return es.map(getDepsFiles(base));
+};
 
 var depsReduce = function () {
-	return es.map(function (file, cb) {
-		var content = String(file.contents);
-		var deps = content.split('\n');
+    return es.map(function (file, cb) {
+        var content = String(file.contents);
+        var deps = content.split('\n');
 
-		deps = deps.map(function (dep){
-			return JSON.parse(dep);
-		});
-		
-		var maps = {};
-		var shim = {
-			angular: 'xxx',
-		}
+        deps = deps.map(function (dep) {
+            return JSON.parse(dep);
+        });
 
-		deps.forEach(function (item){
-			if ( !maps[item.id] ){
+        var maps = {};
 
-				maps[item.id] = item;
+        deps.forEach(function (item) {
+            if (!maps[item.id]) {
+                maps[item.id] = item;
+                delete item.id;
+            }
+        });
 
-				if (shim[item.id]) {
-					maps[item.id]['path'] = shim[item.id];
-				}
+        maps = JSON.stringify(maps, null, 4);
 
-				delete item.id;
-			}
-		});
+        file.contents = new Buffer(maps);
 
-		maps = JSON.stringify(maps);
+        cb(null, file);
+        return null;
+    });
+};
 
-		file.contents = new Buffer(maps);
+module.exports = function (conf, filename) {
+    if (conf) {
+        config = conf;
+    }
 
-		cb(null, file);
-	});
-}
+    var filename = filename || 'map.json';
 
-module.exports = function(filename, options) {
-	var options = {
-
-	}
-
-	var filename = filename || "map.json"
-
-	return es.pipeline(
-		//strip(),
-		depsMap(''),
-		concat(filename),
-		depsReduce()
-	);
+    return es.pipeline(
+        // strip(),
+        depsMap(''),
+        concat(filename),
+        depsReduce()
+    );
 };
